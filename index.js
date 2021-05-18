4b6a1c3dca459445d1a056a7e5245b6e8cea671e
mychal <mychal9251@gmail.com>
2021-05-18 16:30:15 -0700

    uupdates to HTML

---
+++ index.js
@@ -0,0 +1,6 @@
+var guessingWord = ["tacos", "pizza", "sandwich", "burritos", "pickles", "burgers" "fries", "nachos" ]
+
+// randomly choose a work from the array // 
+// find the out the length 
+// create empty array [].push("_")
+// listen for keydown  with addEventlistener 
